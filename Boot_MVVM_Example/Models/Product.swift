15cb6328faa3f3cfd501157d6dd75f5912782295
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-05-18 21:42:12 +0200

    WebService getProducts() done

---
+++ Boot_MVVM_Example/Models/Product.swift
@@ -0,0 +1,10 @@
+//  Product.swift
+//  Boot_MVVM_Example
+//  Created by Miguel Gallego on 18/5/25.
+import UIKit
+
+struct Product: Decodable {
+    let id: Int
+    let title: String
+    let price: Double
+}
