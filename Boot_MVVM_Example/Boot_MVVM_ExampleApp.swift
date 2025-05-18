cc65a0b26434dd50f9ef73267be96a88bdf5bae6
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-05-18 20:52:33 +0200

    Initial Commit

---
+++ Boot_MVVM_Example/Boot_MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boot_MVVM_ExampleApp.swift
+//  Boot_MVVM_Example
+//
+//  Created by Miguel Gallego on 18/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Boot_MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
